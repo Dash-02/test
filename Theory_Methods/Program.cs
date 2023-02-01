cd50326abc64744ec9f86aab4062395f2af22cc9
Dash <dasha.semivolos@gmail.com>
2023-02-01 18:32:20 +0300

    01.02 instance and static methods

---
+++ Theory_Methods/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace Tasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Methods();
+        }
+        
+        static void Methods()
+        {
+            string name = "Abc"; // экземпляр типа string
+
+            // другой способ создания: 
+            string name2 = new string("Abcd");
+
+            bool containsB = name.Contains('b'); // проверяет, есть ли в name "b"
+            bool containsF = name2.Contains('f'); // 'char', "string"
+            Console.WriteLine($"Name contains 'b'? - {containsB}, and Name2 contains 'f'? - {containsF}");
+
+            string abc = string.Concat("a", "b", "c"); // Concat (статический метод) вызывается на типе string,
+                                                       // а возвращает экземпляр типа string, записываемый в abc
+            Console.WriteLine(abc);
+
+            Console.WriteLine(int.MinValue); // статический член, вызывается на прямую на типе
+
+            int x = 1;
+            string xStr = x.ToString();
+            Console.WriteLine($"Number = {x}, String = {xStr}");
+        }
+    }
+}
