ee6909d97deca96ee8bff3587901d4cb7b8b7f5a
Dash <dasha.semivolos@gmail.com>
2023-02-01 18:36:51 +0300

    01.02 empty lines

---
+++ Theory_Empty_Line/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace Tasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EmptyLine();
+        }
+        
+        static void EmptyLine()
+        {
+            string empty = "";
+            string whiteSpaced = " ";
+            string notEmpty = " n";
+            string nullStr = null; //null - отсутствие экземпляра
+
+            Console.WriteLine("Is Null or Empty?");
+
+            bool isNullOrEmpty = string.IsNullOrEmpty(nullStr);
+            Console.WriteLine(isNullOrEmpty);
+
+            isNullOrEmpty = string.IsNullOrEmpty(whiteSpaced);
+            Console.WriteLine(isNullOrEmpty);
+
+            isNullOrEmpty = string.IsNullOrEmpty(notEmpty);
+            Console.WriteLine(isNullOrEmpty);
+
+            isNullOrEmpty = string.IsNullOrEmpty(empty);
+            Console.WriteLine(isNullOrEmpty);
+        }
+    }
+}
