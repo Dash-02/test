9ab8d8bcbf61c322fbafe465cf89c12514c794b7
Dash <dashasemivolos@gmail.com>
2023-02-03 22:39:20 +0300

    03.02 formatting

---
+++ Theory_Formatting/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace Tasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Formatting();
+        }
+        
+        static void Formatting()
+        { 
+
+        }
+    }
+}
