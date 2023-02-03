4f59a3695e43182defe7634867bbcda80144dd61
Dash <dashasemivolos@gmail.com>
2023-02-03 22:32:07 +0300

    03.02 empty line

--- Theory_Empty_Line/Program.cs
+++ Theory_Empty_Line/Program.cs
@@ -13,6 +13,8 @@ namespace Tasks
         
         static void EmptyLine()
         {
+            string str = string.Empty; // выведет ""
+
             string empty = "";
             string whiteSpaced = " ";
             string notEmpty = " n";
@@ -21,17 +23,30 @@ namespace Tasks
             Console.WriteLine("Is Null or Empty?");
 
             bool isNullOrEmpty = string.IsNullOrEmpty(nullStr);
-            Console.WriteLine(isNullOrEmpty);
+            Console.WriteLine(isNullOrEmpty); //true
 
-            isNullOrEmpty = string.IsNullOrEmpty(whiteSpaced);
-            Console.WriteLine(isNullOrEmpty);
+            isNullOrEmpty = string.IsNullOrEmpty(whiteSpaced); 
+            Console.WriteLine(isNullOrEmpty); //false
 
-            isNullOrEmpty = string.IsNullOrEmpty(notEmpty);
-            Console.WriteLine(isNullOrEmpty);
+            isNullOrEmpty = string.IsNullOrEmpty(notEmpty); 
+            Console.WriteLine(isNullOrEmpty); //false
 
             isNullOrEmpty = string.IsNullOrEmpty(empty);
-            Console.WriteLine(isNullOrEmpty);
-	    Console.WriteLine();
+            Console.WriteLine(isNullOrEmpty); //true
+
+            Console.WriteLine("Is Null or WhiteSpace?");
+
+            bool isNullOrWhitespace = string.IsNullOrWhiteSpace(nullStr); 
+            Console.WriteLine(isNullOrWhitespace); //true
+
+            isNullOrWhitespace = string.IsNullOrWhiteSpace(whiteSpaced); 
+            Console.WriteLine(isNullOrWhitespace); //true
+
+            isNullOrWhitespace = string.IsNullOrWhiteSpace(notEmpty); 
+            Console.WriteLine(isNullOrWhitespace); //false
+
+            isNullOrWhitespace = string.IsNullOrWhiteSpace(empty);
+            Console.WriteLine(isNullOrWhitespace); //true
         }
     }
 }
