97193ac02e358a9d0692352293566d5b76a2c019
Dash <dashasemivolos@gmail.com>
2023-02-03 22:40:45 +0300

    03.02 string builder

---
+++ Theory_StrBuild/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace Tasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StrBuild();
+        }
+        
+        static void StrBuild() 
+        { 
+            StringBuilder sb = new StringBuilder();
+            // для наполнения можно вызвать экземплярные методы
+            sb.Append("My ");
+            sb.Append("name ");
+            sb.Append("is ");
+            sb.Append("Joe ");
+
+            sb.AppendLine(".");
+            sb.AppendLine("Hi!");
+
+            string str = sb.ToString();
+            Console.WriteLine(str);
+        }
+    }
+}
