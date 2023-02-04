a19a645001bd30766aa792d370f7220d87d3ced5
Dash <dashasemivolos@gmail.com>
2023-02-04 21:47:12 +0300

    04.02 string comparison

---
+++ Theory_StrComparison/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace Tasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StrComparison();
+        }
+       
+        static void StrComparison() 
+        {
+            string str1 = "abc";
+            string str2 = "abc";
+
+            bool areEqual = string.Equals(str1, str2, StringComparison.Ordinal);
+            Console.WriteLine(areEqual);
+
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.Magenta;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            Console.WriteLine("New Style");  
+        }
+    }
+}
