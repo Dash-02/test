537638f18b0d7f65a807fd575047a7776a0e672b
Dash <dashasemivolos@gmail.com>
2023-02-04 21:46:31 +0300

    04.02 formatting string

--- Theory_Formatting/Program.cs
+++ Theory_Formatting/Program.cs
@@ -14,7 +14,24 @@ namespace Tasks
         
         static void Formatting()
         { 
+            string str = "My \nName";
+            Console.WriteLine(str);
 
+            //метод, который будет корректно отображаться в любой ос
+            string str1 = $"My {Environment.NewLine}Name";
+            Console.WriteLine(str1);
+
+            string str2 = @"C:\ghf\ghfg"; // "\" не нужно экранировать
+            Console.WriteLine(str2);
+
+            double answer = 76;
+            string result = string.Format("{0:f}", answer);
+            Console.WriteLine(result); //76
+
+            string result2 = string.Format("{0:f4}", answer);
+            Console.WriteLine(result2); //0076
+            Console.WriteLine(answer.ToString("f2"));
+            Console.WriteLine($"{answer:f2}");
         }
     }
 }
