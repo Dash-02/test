2bfd41a835ca28cfae57699c85b230fe09e03387
Dash <dashasemivolos@gmail.com>
2023-02-08 22:09:40 +0300

    08.02 tasks

---
+++ Tasks/HeronsFormula.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace Tasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HeronsFormula();
+        }
+        
+        static void HeronsFormula() 
+        {
+            Console.Write("Введите стороны треугольника: ");
+
+            double a = Double.Parse(Console.ReadLine());
+            double b = Double.Parse(Console.ReadLine());
+            double c = Double.Parse(Console.ReadLine());
+
+            double p = (a + b + c)/2;
+            double HeronsFormula = Math.Sqrt(p*(p-a)*(p-b)*(p-c));
+
+            Console.WriteLine($"Площадь треугольника равна: {HeronsFormula}");
+        }
+    }
+}
---
+++ Tasks/UserProfile.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace Tasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UserProfile();
+        }
+        
+        static void UserProfile() 
+        {
+            Console.Write("Введите вашу фамилию: ");
+            string surname = Console.ReadLine();
+
+            Console.Write("Введите ваше имя: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Укажите возраст: ");
+            int age = int.Parse(Console.ReadLine());
+
+            Console.Write("Ваш вес (в кг): ");
+            double weight = Double.Parse(Console.ReadLine());
+
+            Console.Write("Ваш рост (в м): ");
+            double height = Double.Parse(Console.ReadLine());
+
+            double bmi = weight  / (height * height);
+
+            string profile =
+                $"Your profile:{Environment.NewLine}"
+                + $"Full Name: {surname} {name} {Environment.NewLine}"
+                + $"Age: {age} {Environment.NewLine}"
+                + $"Weight: {weight} {Environment.NewLine}"
+                + $"Height: {height} {Environment.NewLine}"
+                + $"Body Mass Index: {bmi} {Environment.NewLine}";
+
+            Console.WriteLine(profile);
+
+            //Console.WriteLine($"Your profile: \nFull Name: {surname} {name}"
+            //+ $"\nAge: {age}\nWeight: {weight}\nHeight: {height}"
+            //+ $"\nBody Mass Index: {bmi}");
+        }
+    }
+}
