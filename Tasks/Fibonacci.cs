fcb94bb6f5b71939d5085bd17c6066ac87a3c1ea
Dash <dashasemivolos@gmail.com>
2023-02-15 22:16:19 +0300

    15.02 Fibonacci numbers

---
+++ Tasks/Fibonacci.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Diagnostics.Metrics;
+
+namespace Tasks_second
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fibonacci();
+        }
+        
+        static void Fibonacci() 
+        {
+            int f = 0;
+            int f1 = 1;
+            
+            Console.Write("Enter the number of fibonacci numbers: ");
+
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] F = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                int fib = f + f1;
+               
+                F[i] = fib;
+
+                f1 = f;
+                f = fib;
+                
+                Console.Write($"{F[i]} ");
+            }
+        }
+    }
+}
